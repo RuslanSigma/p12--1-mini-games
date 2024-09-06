68bb393c42a3a8f6b7937d5e859993bd345d0574
RuslanSigma <mirrorroyale4@gmail.com>
2024-09-06 20:11:45 +0300

    hi
    lol

---
+++ ігри/ігри/Pages/game1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ігри.Pages
+{
+    public class game1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
